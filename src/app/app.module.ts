6f7bd52c900d2509deb2e50d4d64a4561dcad509
Mauro Tranchina <mauro90trn@gmail.com>
2021-10-14 00:21:39 +0200

    nuovo.commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project/project.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+   
   ],
   imports: [
     BrowserModule
