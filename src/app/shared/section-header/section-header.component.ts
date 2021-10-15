772a2347301c7c440d5987237a99cfceba163467
Mauro Tranchina <mauro90trn@gmail.com>
2021-10-15 19:19:55 +0200

    services

--- src/app/shared/section-header/section-header.component.ts
+++ src/app/shared/section-header/section-header.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Button {
+  label: string;
+  link: string;
+  type: 'btn-link' | 'btn-primary'
+}
+
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
@@ -8,10 +14,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SectionHeaderComponent implements OnInit {
   @Input() title = '';
 
-  @Input() buttonLabel = 'Vai a ...';
-  @Input() buttonClass = 'btn-link';
-  @Input() buttonLink = '';
+ _button: Button = { label: 'Vai a ...', link:'', type: 'btn-link'};
 
+ @Input() set button(value: Partial<Button>) {
+   this._button = { ...this._button, ...value };
+ }
 
   constructor() { }
 
