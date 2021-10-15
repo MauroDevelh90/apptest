eca83d960a30f3d05fef682ab1382b4c2a9c1ea0
Mauro Tranchina <mauro90trn@gmail.com>
2021-10-15 14:21:38 +0200

    section-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { ProjectContainerComponent } from './project/project-container/project-c
 import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
 import { ProjectListComponent } from './project/project-list/project-list.component';
 import { ProjectFormComponent } from './project/project-form/project-form.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SectionHeaderComponent } from './shared/section-header/section-header.component';
 
 
 @NgModule({
@@ -16,11 +20,15 @@ import { ProjectFormComponent } from './project/project-form/project-form.compon
     ProjectDetailComponent,
     ProjectListComponent,
     ProjectFormComponent,
+    HomeComponent,
+    NavbarComponent,
+    SectionHeaderComponent,
    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/section-header/section-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  templateUrl: './section-header.component.html',
+  styleUrls: ['./section-header.component.css']
+})
+export class SectionHeaderComponent implements OnInit {
+  @Input() title = '';
+
+  @Input() buttonLabel = 'Vai a ...';
+  @Input() buttonClass = 'btn-link';
+  @Input() buttonLink = '';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
