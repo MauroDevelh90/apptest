903fb45fb539177d0a39137092810888541d9546
Mauro Tranchina <mauro90trn@gmail.com>
2021-10-20 00:17:30 +0200

    http

--- src/app/project/project-container/project-container.component.ts
+++ src/app/project/project-container/project-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import { Project } from '@app/models/Project';
-import { LogService } from '@app/shared/log.service';
+
+import { Observable, Subscription } from 'rxjs';
 import { ProjectService } from '../project.service';
 
 @Component({
@@ -9,25 +10,33 @@ import { ProjectService } from '../project.service';
   templateUrl: './project-container.component.html',
   styleUrls: ['./project-container.component.css']
 })
-export class ProjectContainerComponent implements OnInit {
+export class ProjectContainerComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
   selectedProject!: Project;
 
-  projects: Project[] = [];
+  // projects: Project[] = [];
+  projects$!: Observable<Project[]>;
      
   constructor(private projectService: ProjectService) {}
   
 
   ngOnInit(): void {
-    this.projects = this.projectService.getAll();
+    this.projects$ = this.projectService.getAll();
   }
 
   selectProject(project: Project) {
-    this.selectedProject = this.projectService.get(project.id);
+   this.subscription = this.projectService.get(project.id).subscribe(data => {
+      this.selectedProject = data;
+    })
   }
 
   submitProjectForm (project: Project) {
-    this.projectService.add(project);
+    this.projectService.add(project).subscribe(data => this.projects$ = this.projectService.getAll());
    
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,65 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '@app/models/Project';
+import { LogService } from '@app/shared/log.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  projects: Project[] = [
-  {
-    id: 1,
-    code: 'NHusYJl',
-    name: 'Progetto Alpha',
-    description: 'Lorem ipsum dolor sit amet.',
-    start: new Date(2019, 1, 30),
-    end: new Date(2019, 3, 15),
-    priority: 'medium',
-    done: true,
-    tasks: []
-  },
-  {
-    id: 2,
-    code: 'SJieYKl',
-    name: 'Progetto Beta',
-    description: 'Lorem ipsum dolor sit amet.',
-    start: new Date(2019, 3, 30),
-    end: new Date(2019, 6, 15),
-    priority: 'low',
-    done: true,
-    tasks: []
-  },
-  {
-    id: 3,
-    code: 'POjeGBs',
-    name: 'Progetto Gamma',
-    description: 'Lorem ipsum dolor sit amet.',
-    start: new Date(2019, 8, 15),
-    priority: 'low',
-    done: false,
-    tasks: []
-  },
-]
+constructor(private httpClient: HttpClient,  private logService: LogService) {}
 
-
-constructor() {}
-
-getAll(): Project[] {
-  return this.projects;
+getAll(): Observable<Project[]> {
+  return this.httpClient.get<Project[]>('http://localhost:3000/projects')
+  .pipe(
+    tap((data) => this.logService.log(`getAll Eseguito ${data}`))
+  );
 }
+ 
 
-add(project: Project): void {
-  this.projects.push({
+add(project: Project): Observable<Project> {
+  const projectToAdd = {
     ...project,
-    id: this.projects.length,
     code: Math.random().toString(36).replace('0.', '').substring(2, 9),
     done: false,
-    tasks: [],
-  });
+    tasks: []
+  };
+  return this.httpClient.post<Project>('http://localhost:3000/projects', projectToAdd).pipe(
+  tap((data) => this.logService.log(`add Eseguito ${data}`))
+  );
 }
 
-get(id: number): Project {
-  return this.projects.find(project => project.id === id) as Project;
+get(id: number): Observable<Project> {
+  return this.httpClient.get<Project>(`http://localhost:3000/projects/${id}`).pipe(
+    tap((data) => this.logService.log(`get Eseguito ${data}`))
+    );
 }
 
 }
